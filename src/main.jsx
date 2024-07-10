edd65f329a8a3121f1c6b54b57e31c6727d3f6ab
tldayan <110288123+tldayan@users.noreply.github.com>
2024-07-10 18:55:22 +0400

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,15 @@ ReactGA.initialize("G-6Z3LEDT59J");
 const stytchClient = new StytchUIClient(import.meta.env.VITE_STYTCH_PUBLIC_TOKEN, {
   cookieOptions: {
     opaqueTokenCookieName: 'stytch_session',
-    jwtCookieName: 'stytch_sesssssion_jwt',
+    jwtCookieName: 'stytch_session_jwt',
+    path: '/',
+    domain: '.vercel.app', // This covers both subdomains
     availableToSubdomains: true,
-    secure: true,
-    sameSite: 'None' // Adjust to 'Lax' or 'Strict' based on your requirements
+    secure: true, // Ensure this is true for HTTPS
+    sameSite: 'None' // Required for cross-site cookies
   }
 });
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
