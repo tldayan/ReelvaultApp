4227aba4d49c9ab8a1e27b5bc55a62109b027f50
tldayan <110288123+tldayan@users.noreply.github.com>
2024-09-30 17:51:11 +0400

    Fix

--- Components/AllMovies/AllMovies.jsx
+++ Components/AllMovies/AllMovies.jsx
@@ -3,15 +3,13 @@ import {Outlet} from "react-router-dom"
 import {NavLink} from "react-router-dom"
 import { CategoryButtonsContainer } from './AllMovies.styles'
 import ResumeShowsContainer from '../ResumeShowsContainer/ResumeShowsContainer'
-import { useStytchSession } from '@stytch/react'
 
 
 export default function AllMovies() {
-  const {session} = useStytchSession()
 
   return (
     <>
-    {session && <ResumeShowsContainer />}
+    <ResumeShowsContainer />
     <CategoryButtonsContainer>
       <NavLink className='category_buttons' to="/">Popular Shows</NavLink>
       <NavLink className='category_buttons' to="/popular">Popular Movies</NavLink>
