7c5e3412bae9f73020cd40e6b37480114e8a3e83
tldayan <110288123+tldayan@users.noreply.github.com>
2024-10-14 19:04:59 +0400

    implemented useMemo for Optimization

--- Components/SearchBar/SearchBar.jsx
+++ Components/SearchBar/SearchBar.jsx
@@ -1,126 +1,130 @@
-import React from 'react'
-import { getMovieSearchData, getShowSearchData} from '../APIs/Api'
-import { useEffect, useRef, useState } from 'react'
-import sadFace from "../../assets/sad_face.png"
-import searchIcon from "../../assets/search_icon.svg"
-import SearchResultCard from '../SearchResultCard/SearchResultCard'
-import { StyledSearchBar } from './SearchBar.styles'
+import React, { useEffect, useRef, useState, useMemo } from 'react';
+import { getMovieSearchData, getShowSearchData } from '../APIs/Api';
+import sadFace from "../../assets/sad_face.png";
+import searchIcon from "../../assets/search_icon.svg";
+import SearchResultCard from '../SearchResultCard/SearchResultCard';
+import { StyledSearchBar } from './SearchBar.styles';
 
 function removeSpecialCharacters(input) {
   return input.replace(/[:\-]/g, "").toLowerCase();
 }
 
-
 export default function SearchBar() {
+  const searchInput = useRef(null);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchDataLoading, setSearchDataLoading] = useState(false);
+  const searchField = useRef(null);
+  const searchList = useRef(null);
 
-  const searchInput = useRef(null)
-  const [search,setSearch] = useState("")
-  const [searchResults, setSearchResults] = useState([])
-  const [searchDataLoading, setSearchDataLoading] = useState(false)
-  const searchField = useRef(null)
-  const searchList = useRef(null)
-
-
-  const fetchSearchData = async(entitySearch) => {
+  const fetchSearchData = async (entitySearch) => {
 
     try {
-  
-      const [movieResponse, showResponse] = await Promise.all([getMovieSearchData(entitySearch), getShowSearchData(entitySearch)]);
-  
+      const [movieResponse, showResponse] = await Promise.all([
+        getMovieSearchData(entitySearch),
+        getShowSearchData(entitySearch)
+      ]);
+
       const searchData = [...movieResponse, ...showResponse];
-  
-      let filteredSearchData = searchData.filter((eachResult) => {
-        const title = removeSpecialCharacters(eachResult.original_name || eachResult.original_title);
-        const searchQuery = removeSpecialCharacters(entitySearch);
-        return title.includes(searchQuery);
-      }).sort((a,b) => b.popularity - a.popularity);
-  
-        setSearchResults(filteredSearchData)
-        setSearchDataLoading(false)
-  
+      setSearchResults(searchData);
+      setSearchDataLoading(false);
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
+
+
+
+  const filteredResults = useMemo(() => {
+    if (searchResults.length === 0 || search === "") return [];
+
+    return searchResults.filter((eachResult) => {
+      const title = removeSpecialCharacters(eachResult.original_name || eachResult.original_title);
+      const searchQuery = removeSpecialCharacters(search);
+      return title.includes(searchQuery);
+    }).sort((a, b) => b.popularity - a.popularity);
+  }, [searchResults, search]);
+
 
 
   useEffect(() => {
-
-    if(search === "" ) { 
-      setSearchDataLoading(false)
-      return
+    if (search === "") {
+      setSearchDataLoading(false);
+      return;
     }
 
     const searchTimer = setTimeout(() => {
-      setSearchDataLoading(true)
-    let entitySearch = undefined
-  
-    if(search[search.length - 1] === " ") {
-      entitySearch = search.trim()
-    } else {
-      entitySearch = search
-    }
+      setSearchDataLoading(true);
+      let entitySearch = search.trim();
 
-      fetchSearchData(entitySearch)
-    },500)
+      fetchSearchData(entitySearch);
+    }, 500);
 
-    return () => clearTimeout(searchTimer)
+    return () => clearTimeout(searchTimer);
+  }, [search]);
 
-  },[search])
 
-  
- function scrollToField() {
+
+  function scrollToField() {
     const offset = 80;
     const element = searchField.current;
 
     if (element) {
-        const elementTop = element.getBoundingClientRect().top;
-        window.scrollBy({ top: elementTop - offset});
+      const elementTop = element.getBoundingClientRect().top;
+      window.scrollBy({ top: elementTop - offset });
     }
 
-    if(search !== "") {
-      searchList?.current.classList.add("active")
+    if (search !== "") {
+      searchList.current.classList.add("active");
     }
-
   }
 
-  useEffect(() => {
 
+
+  useEffect(() => {
     function handleClickOutsideSearchList(event) {
-      if(searchInput.current && !searchInput.current.contains(event.target)) {
-        searchList.current.classList.remove("active")
+      if (searchInput.current && !searchInput.current.contains(event.target)) {
+        searchList.current.classList.remove("active");
       }
     }
 
-    document.addEventListener("mousedown", handleClickOutsideSearchList)
-    return () => document.removeEventListener("mousedown", handleClickOutsideSearchList)
+    document.addEventListener("mousedown", handleClickOutsideSearchList);
+    return () => document.removeEventListener("mousedown", handleClickOutsideSearchList);
+  }, []);
 
-  },[])
 
 
   return (
     <>
       <p className='slogan'>ReelVault ~ Where the World Watches.</p>
-    
-      <StyledSearchBar ref={searchField} >
+      <StyledSearchBar ref={searchField}>
         <input
-            className='search_field'
-            type="text"
-            placeholder='Search movies & shows...'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            ref={searchInput}
-            onClick={scrollToField}
-            onFocus={scrollToField}
-            spellCheck="false"
+          className='search_field'
+          type="text"
+          placeholder='Search movies & shows...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          ref={searchInput}
+          onClick={scrollToField}
+          onFocus={scrollToField}
+          spellCheck="false"
         />
         <div ref={searchList} className={`search_list ${search !== "" ? "active" : null}`}>
-            {searchDataLoading ? <div className="race-by"></div> : searchResults.length ? searchResults.map(eachResult => {
-            return  <SearchResultCard setSearch={setSearch} key={eachResult.id} eachResult={eachResult} />
-            }) : <div className='no_search_results_container'><img src={sadFace} alt="" /><p className='no_search_result'>No results for "{search}"</p></div>}
+          {searchDataLoading ? (
+            <div className="race-by"></div>
+          ) : filteredResults.length ? (
+            filteredResults.map(eachResult => {
+              return <SearchResultCard setSearch={setSearch} key={eachResult.id} eachResult={eachResult} />;
+            })
+          ) : (
+            <div className='no_search_results_container'>
+              <img src={sadFace} alt="" />
+              <p className='no_search_result'>No results for "{search}"</p>
+            </div>
+          )}
         </div>
         <img className="search_icon" src={searchIcon} alt="searchIcon" />
       </StyledSearchBar>
     </>
-  )
+  );
 }
