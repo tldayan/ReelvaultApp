da75646fb9e72d33203075d3f41889535792f19e
tldayan <110288123+tldayan@users.noreply.github.com>
2024-11-11 15:40:11 +0400

    Revert

--- Components/Movie-ShowDetails/ShowDetails.jsx
+++ Components/Movie-ShowDetails/ShowDetails.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useLayoutEffect, useRef} from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useLayoutEffect, useRef, useState} from "react";
+import { useDispatch} from "react-redux";
 import { EpisodeLinkActions } from "../store/EpisodeLinkSlice";
 import { ShowDetailsContainer } from "./Movie-ShowDetails.styles";
 import Reviews from "../Reviews/Reviews";
 import Recommended from "../Recommended/Recommended";
-import { showsWatchlistActions } from "../store/showsWatchlistSlice";
 import eye from "../../assets/eye.png"
 import { UpdateUserShowDetails } from "../APIs/mongo/UpdateUserShowDetails";
 import { getUserShowDetails } from "../APIs/mongo/UserShowDetail";
@@ -24,6 +23,9 @@ export default function ShowDetails({showDispatch, showId,showDataLoading,season
   let episodeButton = useRef(null)
   let episodeListContainer = useRef(null) 
   const navigate = useNavigate()
+  const [userWatchlist,setUserWatchlist] = useState(() => {
+    return JSON.parse(localStorage.getItem('getUserWatchlist')) || []
+  })
 
   useEffect(() => {
     if(seasonList && seasonList.length > 0) {
@@ -148,17 +150,69 @@ export default function ShowDetails({showDispatch, showId,showDataLoading,season
   }, [episodeList, selectedEpisode, selectedSeason]);
   
 
-  const watchlist = useSelector((state) => state.showsWatchlist)
+  const isInWatchlist = userWatchlist.some(eachEntity => eachEntity.entityId == showId);
 
-  function handleWatchlist() {
-    
-    if(watchlist.some(eachEntity => eachEntity?.showId == showId)) {
-      dispatch(showsWatchlistActions.removeFromWatchlist(showId))
+  async function handleWatchlist() {
+      
+    if(isInWatchlist) {
+
+      try {
+
+        const deleteShowRequest = await fetch(`https://reelvault-server.vercel.app/deleteUserWatchlist?entityId=${showId}&userId=${userId}`, {
+          method : "DELETE"
+        })
+
+        const deleteShowRequestResponse = await deleteShowRequest.json()
+
+        if(deleteShowRequestResponse.ok) {
+          let updatedWatchList = userWatchlist.filter(eachEntity => eachEntity.entityId !== showId)
+          setUserWatchlist(updatedWatchList)
+          localStorage.setItem("userWatchlist", JSON.stringify(updatedWatchList));
+        }
+        
+      } catch (err) {
+        console.log(err.message)
+      }
+
     } else {
-      dispatch(showsWatchlistActions.addToWatchlist(showData))
+
+      let entityObject = {
+        entityId: showId,
+        entityName: showData.name,
+        entityReleaseDate: showData.first_air_date,
+        entityPosterUrl: showData.poster_path,
+        entityType: "Show",
+        entityDescription: showData.overview
+      }
+
+      try {
+
+        const addShowRequest = await fetch(`https://reelvault-server.vercel.app/addUserWatchlist`, {
+          method: "POST",
+          body: JSON.stringify({entityObject : entityObject, userId: userId})
+        })
+
+        const addShowRequestResponse = await addShowRequest.json()
+
+        if(addShowRequestResponse.ok) {
+
+          let updatedUserWatchlist = [...userWatchlist, {entityObject}]
+
+          setUserWatchlist(updatedUserWatchlist)
+          localStorage.setItem("userWatchlist", JSON.stringify(updatedUserWatchlist));
+        }
+        
+      } catch(err) {
+        console.log(err.message)
+      } 
     }
   }
 
+  useEffect(() => {
+    const storedWatchlist = JSON.parse(localStorage.getItem('userWatchlist')) || [];
+    setUserWatchlist(storedWatchlist);
+  }, []);
+  
   
   const shareEntity = () => {
     const shareData = {
@@ -215,13 +269,11 @@ export default function ShowDetails({showDispatch, showId,showDataLoading,season
               )}
             
             </div>
-            
-
           </div>
           <div className="buttons_container">
             {showTrailerKey && <button onClick={() => window.open(`https://www.youtube.com/watch?v=${showTrailerKey}`)} className={`trailer_btn`}><img className="eye_icon" src={eye} loading="lazy"></img>Trailer</button>}
             <button className="share_btn" onClick={shareEntity}>Share</button>
-            <button className={`watchlist_btn ${watchlist.some(eachEntity => eachEntity.showId == showId) ? "active" : ""}`} onClick={handleWatchlist}>{watchlist.some(eachEntity => eachEntity.showId == showId) ? "In Watchlist" : "+ Watchlist"}</button>
+            <button className={`watchlist_btn ${isInWatchlist ? "active" : ""}`} onClick={handleWatchlist}>{isInWatchlist ? "In Watchlist" : "+ Watchlist"}</button>
           </div>
         </div>
         <div className="shows_list_container">
